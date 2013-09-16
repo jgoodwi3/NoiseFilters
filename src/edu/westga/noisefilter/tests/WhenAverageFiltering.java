8f84b931808b5452ccb2e20c7337f749915d2d88
Dhruval Darji <dhruvaldarji@gmail.com>
2013-09-16 13:22:47 -0400

    Finished part 1, need to test.
    Added method clear to SensorData, added two new methods to When average
    filtering.

--- src/edu/westga/noisefilter/tests/WhenAverageFiltering.java
+++ src/edu/westga/noisefilter/tests/WhenAverageFiltering.java
@@ -21,9 +21,11 @@ public class WhenAverageFiltering {
 		filter = dataSet.setFilter(new AveragingFilter());
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Test
 	public void averageOfThree100sShouldBe100() {
+		// Clear the dataSet just in case.
+		this.dataSet.clear();
+		
 		this.dataSet.addSample(100.00);
 		this.dataSet.addSample(100.00);
 		this.dataSet.addSample(100.00);
@@ -36,4 +38,43 @@ public class WhenAverageFiltering {
 		double actual = 100.0;
 		assertEquals(expected, actual, Math.abs(expected - actual));
 	}
+	
+	@Test
+	public void averageOfThreeDiffernet() {
+		// Clear the dataSet just in case.
+		this.dataSet.clear();
+		
+		this.dataSet.addSample(50.00);
+		this.dataSet.addSample(100.00);
+		this.dataSet.addSample(150.00);
+		
+		/* This part throws error where it asks you to use delta. You compensate
+		 * by using Math.abs(expected-actual). This value is how much your assertion
+		 * can be off by.
+		 */
+		double expected = dataSet.getFilteredResult();
+		double actual = 100.0;
+		assertEquals(expected, actual, Math.abs(expected - actual));
+	}
+	
+	@Test
+	public void averageOfFiveDiffernet() {
+		// Clear the dataSet just in case.
+		this.dataSet.clear();
+		
+		this.dataSet.addSample(50.00);
+		this.dataSet.addSample(75.00);
+		this.dataSet.addSample(100.00);
+		this.dataSet.addSample(125.00);
+		this.dataSet.addSample(150.00);
+		
+		/* This part throws error where it asks you to use delta. You compensate
+		 * by using Math.abs(expected-actual). This value is how much your assertion
+		 * can be off by.
+		 */
+		double expected = dataSet.getFilteredResult();
+		double actual = 100.0;
+		assertEquals(expected, actual, Math.abs(expected - actual));
+	}
+	
 }
